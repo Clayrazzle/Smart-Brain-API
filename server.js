b5d14de5457080d2b801a9b93f213c5584a8fb9a
System Administrator <root@Clays-MacBook-Pro.local>
2023-05-25 22:28:30 +0900

    app commit

--- server.js
+++ server.js
@@ -45,5 +45,6 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-	console.log(`app is running on port ${process.env.PORT}`)};
+app.listen(process.env.PORT || 3000, () => {
+console.log(`app is running on port ${process.env.PORT}`);
+})
