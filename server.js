5363abdb9de264006398942f33b05896a8639cf6
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-06-05 09:55:24 +0900

    listen

--- server.js
+++ server.js
@@ -47,5 +47,6 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
-})
+  const port = process.env.PORT || 3000;
+  console.log(`app is running on port ${port}`);
+});
