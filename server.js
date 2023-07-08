0a47c8ff280c2b32382b9d88ea7f5315f478f72c
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-07-08 10:07:41 +0900

    catch order

--- server.js
+++ server.js
@@ -28,12 +28,12 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt); });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db); });
+app.put('/image', (req, res) => { image.handleImage(req, res, db); });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res); });
+app.get('/', (req, res) => { res.send(db.users); }); // Place the catch-all route at the end
 
 // Add the test route below
 app.get('/test', (req, res) => {
