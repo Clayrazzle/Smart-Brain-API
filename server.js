003a19b23ce5d585024a33163b2e4543c73a1f94
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-07-08 09:16:09 +0900

    false

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl : {rejectUnauthorized: true },
+    ssl : {rejectUnauthorized: false },
     host: process.env.DATABASE_HOST,
     port: 5432,
     user : process.env.DATABASE_USER,
