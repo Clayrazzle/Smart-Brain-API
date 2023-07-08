55b047ba3348adab08abe999bd74cbf886e94c34
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-07-08 09:08:26 +0900

    ports

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl : {rejectUnauthorized: false },
+    ssl : {rejectUnauthorized: true },
     host: process.env.DATABASE_HOST,
     port: 5432,
     user : process.env.DATABASE_USER,
@@ -32,12 +32,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { res.send('it is working'); });
+app.get('/', (req, res)=> { res.send(db.users) });
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt); });
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db); });
-app.put('/image', (req, res) => { image.handleImage(req, res, db); });
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res); });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db);});
+app.put('/image', (req, res) => { image.handleImage(req, res, db);});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res);});
 
 // Add the test route below
 app.get('/test', (req, res) => {
@@ -52,6 +52,7 @@ app.get('/test', (req, res) => {
     });
 });
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+const port = process.env.PORT || 3000; // Use the provided port or default to 3000
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
