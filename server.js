cd68e91c84a0968ab6923f00bad3704655713100
Clay <clayton.randall1012@gmail.com>
2025-09-23 16:49:23 +0900

    Update for Node 18 + Yarn

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
